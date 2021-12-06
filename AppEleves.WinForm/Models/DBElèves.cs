ddc22a79d503445514e4ee98ec36befe987c10d3
Trésor KAOMBA <tresorkaomba1@gmail.com>
2021-12-06 17:08:49 +0200

    DBElèves

---
+++ AppEleves.WinForm/Models/DBElèves.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppEleves.WinForm.Models
+{
+    public static class DBElèves
+    {
+        public static List<Elèves> lstElèves;
+        // Ajouter un élève à la liste des élèves
+        public static int AddElèves(Elèves c)
+    {
+        if (lstElèves == null)
+        {
+                lstElèves = new List<Elèves>();
+         }
+            c.Id = lstElèves.Any() ? lstElèves.Max(x => x.Id) + 1 : 100; //lambda Expressions
+            lstElèves.Add(c);
+            return c.Id;
+        }
+        // Récuperer la liste des élèves
+        public static List<Elèves> GetListElèves()
+        {
+            return lstElèves;
+        }
+        // Supprimer un élève
+        public static void DeleteElèves(int idElèves)
+        {
+            int index = lstElèves.FindIndex(x => x.Id == idElèves);
+            lstElèves.RemoveAt(index);
+        }
+        // Modifier un élève
+        public static void UpdateElèves(Elèves c)
+        {
+            Elèves rech = lstElèves.FirstOrDefault(x => x.Id == c.Id);
+            rech.NumPerm = c.NumPerm;
+            rech.NomComplet = c.NomComplet;
+            rech.DateNaiss = c.DateNaiss;
+            rech.LieuNaiss = c.LieuNaiss;
+            rech.Sexe = c.Sexe;
+            rech.Classe = c.Classe;
+            rech.Option = c.Option;
+            rech.Niveau = c.Niveau;
+            rech.AnneeScol = c.AnneeScol;
+            rech.NomPere = c.NomPere;
+            rech.NomMere = c.NomMere;
+            rech.Tel = c.Tel;
+            rech.Adresse = c.Adresse;
+            rech.Photo = c.Photo;
+        }
+        // Chercher par nom
+        public static List<Elèves> SearchElèves (string name)
+        {
+            List<Elèves> res = lstElèves.Where(x => x.NomComplet.ToLower().Contains(name.ToLower())).ToList();
+            return res;
+        }
+    }
+}
---
+++ AppEleves.WinForm/Models/Elèves.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppEleves.WinForm.Models
+{
+    public class Elèves
+    {
+        public int Id { get; set; }
+        public string NumPerm { get; set; }
+        public string NomComplet { get; set; }
+        public DateTime DateNaiss { get; set; }
+        public string LieuNaiss { get; set; }
+        public string Sexe { get; set; }
+        public string Classe { get; set; }
+        public string Option { get; set; }
+        public string Niveau { get; set; }
+        public string AnneeScol { get; set; }
+        public string NomPere { get; set; }
+        public string NomMere { get; set; }
+        public string Tel { get; set; }
+        public string Adresse { get; set; }
+        public byte[] Photo { get; set; }
+
+        public Elèves(string numPerm, string nomComplet, DateTime dateNaiss, string lieuNaiss, string sexe, string classe, string option, string niveau, string anneeScol, string nomPere, string nomMere, string tel, string adresse, byte[] photo)
+        {
+            this.NumPerm = numPerm;
+            this.NomComplet = nomComplet;
+            this.DateNaiss = dateNaiss;
+            this.LieuNaiss = lieuNaiss;
+            this.Sexe = sexe;
+            this.Classe = classe;
+            this.Option = option;
+            this.Niveau = niveau;
+            this.AnneeScol = anneeScol;
+            this.NomPere = nomPere;
+            this.NomMere = nomMere;
+            this.Tel = tel;
+            this.Adresse = adresse;
+            this.Photo = photo;
+        }
+        public Elèves()
+        {
+        }
+    }
+}
